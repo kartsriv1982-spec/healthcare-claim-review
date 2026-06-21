03f3cde66b240384341f8c9c9bcc213b1ef9dd4b
DG <ghosh.devashis@gmail.com>
2026-06-21 10:04:21 +0530

    Changes till Query API

---
+++ app/rag/embeddings/embeddingProvider.py
@@ -0,0 +1,14 @@
+from langchain_openai import OpenAIEmbeddings
+
+from config.loadConfig import (OPENAI_EMBEDDING_MODEL)
+from config.loadSecrets import (OPENAI_API_KEY)
+
+
+def get_embedding_model():
+
+    api_key = OPENAI_API_KEY
+
+    return OpenAIEmbeddings(
+        model=OPENAI_EMBEDDING_MODEL,
+        api_key=api_key
+    )
